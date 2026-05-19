27ff17ae116ebce090713041c068259918982d2b
Dastan FC <sliink27@gmail.com>
2026-05-19 18:56:57 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,12 @@
 self.addEventListener('push', function(evento) {
   const datos = evento.data ? evento.data.json() : { title: 'Test', body: 'Notificación' };
-  
   const opciones = {
     body: datos.body || 'Tienes una notificación nueva',
-    icon: '/icon-192.png',
+    icon: '/pruebadetest/icon-192.png',
     vibrate: [200, 100, 200]
   };
-  
   evento.waitUntil(
     self.registration.showNotification(datos.title || '🔔 Recomendador', opciones)
-//                                                               
   );
 });
 
